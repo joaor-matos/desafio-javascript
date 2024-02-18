878203dbb335b8e760f07f6c39fbad893dafe81a
joaor-matos <dev.joaormatos@gmail.com>
2024-02-18 19:38:08 -0300

    Desafio finalizado

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("ola mundo")
+let nomeHeroi = "Ruka Fukagawa"
+let xpHeroi = 10000
+
+if (xpHeroi <= 1000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Ferro")
+} 
+else if (xpHeroi >= 1001 && xpHeroi <= 2000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Bronze")
+} 
+else if (xpHeroi >= 2001 && xpHeroi <= 5000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Prata")
+} 
+else if (xpHeroi >= 5001 && xpHeroi <= 7000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Ouro")
+} 
+else if (xpHeroi >= 7001 && xpHeroi <= 8000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Platina")
+} 
+else if (xpHeroi >= 8001 && xpHeroi <= 9000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Ascendente")
+} 
+else if (xpHeroi >= 9001 && xpHeroi <= 10000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Imortal")
+} 
+else if (xpHeroi >= 10001){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Radiante")
+}
